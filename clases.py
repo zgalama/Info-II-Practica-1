97591103e025c8006559dbfde346cfeeb0d1acd3
Félix Martínez <felixmaral@MacBook-Air-de-Felix.local>
2023-10-10 21:29:07 +0200

    Artillero, primera versión

--- clases.py
+++ clases.py
@@ -71,8 +71,7 @@ class Inteligencia(Personaje):
                 print(ene, 'en', pos)
             print('\n')
 
-class Francotirador(Personaje): #-- Terminar Francotirador
-
+class Francotirador(Personaje):
     def __init__(self):
         super().__init__()
         self.id = 'Francotirador'
@@ -82,6 +81,50 @@ class Francotirador(Personaje): #-- Terminar Francotirador
         for enemigo in equipo_enemigo:
             if enemigo.posicion == posicion:
                 enemigo.vida_actual = 0
+
+class Artillero(Personaje):
+    def __init__(self):
+        super().__init__()
+        self.id = 'Artillero'
+
+    def habilidad(self, posicion : str, opo : Jugador):
+        letras_casillas = ('a', 'b', 'c', 'd')
+        numeros_casillas = ('1', '2', '3', '4')
+
+        casillas_exploradas = [posicion]
+
+        for letra in letras_casillas:
+            if letra == posicion[0]:
+                for numero in numeros_casillas:
+                    if numero == posicion[1]:
+                        idx_letra = letras_casillas.index(letra)
+                        idx_numero = numeros_casillas.index(numero)
+                        try:
+                            cas2 = letras_casillas[idx_letra + 1] + numero
+                            cas3 = letra + numeros_casillas[idx_numero + 1]
+                            cas4 = letras_casillas[idx_letra + 1] + numeros_casillas[idx_numero + 1]
+                            casillas_exploradas.extend([cas2, cas3, cas4])
+                            casillas_exploradas  # -- Lista con las casillas a explorar
+                        except:
+                            error = print('La ubicacion seleccionada no es valida para un area 2x2\n')
+                            return error
+
+        dañados = {}
+
+        for enemigo in opo.equipo:
+            if enemigo.posicion in casillas_exploradas:
+                enemigo.vida_actual = enemigo.vida_actual - 1
+                dañados[enemigo.id] = enemigo.vida_actual
+                print(f'El {enemigo.id} ha sido dañado en {enemigo.posicion}. Vida restante [{enemigo.vida_actual}]')
+
+        if not dañados:
+            print('No se ha dañado a ningún enemigo\n')
+
+
+
+
+
+
 
 
 
